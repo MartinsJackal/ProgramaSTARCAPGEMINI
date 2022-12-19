ba7e2083c3affaf53678f9d79156db79d86562f4
João Victor Martins <63604234+MartinsJackal@users.noreply.github.com>
2022-12-19 11:38:05 -0300

    Delete Projects/ExercicioPOO_2 directory

--- Projects/ExercicioPOO_2/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        
-        invoice inv = new invoice(1 , "MousePad"  ,-10 , 50.0f);
-
-        System.out.println("Valor Total = " + inv.getInvoiceAmount());
-    }
-}
